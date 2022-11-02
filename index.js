cfd143e4f998ae906df224ef542ac209ae494afa
amyhskwon <amyhskwon@gmail.com>
2022-11-02 08:32:52 -0700

    commit tree a21279d8a5ad4eca563bf55a73bdf7d12cce1d8e

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(array) {
+    const newArray = array.slice(0, 2);
+    return newArray;
+}
+
+const returnLastTwoDrivers = function(array) {
+    const newArray = array.slice(2);
+    return newArray;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(number) {
+    return function(fare = 25) {fare * number};
+}
